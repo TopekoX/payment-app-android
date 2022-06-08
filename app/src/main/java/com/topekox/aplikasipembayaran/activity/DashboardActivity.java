adef58f2c1c3d71a324a0e9f5a771bdc7d9512ce
Ucup Topekox <ucup.topekox@gmail.com>
2022-06-08 21:48:18 +0800

    Add Firebase Cloud Messaging Service

--- app/src/main/java/com/topekox/aplikasipembayaran/activity/DashboardActivity.java
+++ app/src/main/java/com/topekox/aplikasipembayaran/activity/DashboardActivity.java
@@ -9,8 +9,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.topekox.aplikasipembayaran.R;
 import com.topekox.aplikasipembayaran.dao.TagihanDao;
 import com.topekox.aplikasipembayaran.domain.Tagihan;
@@ -50,6 +54,26 @@ public class DashboardActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Firebase Action
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        // String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, "Token: " + token);
+                        // Toast.makeText(DashboardActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
 
         insertDummyData();
         getSupportFragmentManager().beginTransaction()
---
+++ app/src/main/java/com/topekox/aplikasipembayaran/service/FirebaseHandleMessagingService.java
@@ -0,0 +1,104 @@
+package com.topekox.aplikasipembayaran.service;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.topekox.aplikasipembayaran.R;
+import com.topekox.aplikasipembayaran.activity.DashboardActivity;
+
+/**
+ * see more: https://firebase.google.com/docs/cloud-messaging/android/client
+ */
+public class FirebaseHandleMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FIREBASE_MSG_SERVICE";
+
+    /**
+     * see more: https://firebase.google.com/docs/cloud-messaging/android/receive
+     * @param remoteMessage
+     */
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Title: " + remoteMessage.getNotification().getTitle());
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        sendNotification(remoteMessage.getFrom(), remoteMessage.getNotification().getTitle(),
+                remoteMessage.getNotification().getBody());
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+
+    private void sendNotification(String from, String title, String body) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(FirebaseHandleMessagingService.this.getApplicationContext(), from + " -> " + title +
+                        " -> " + body, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, DashboardActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = "My Channel ID";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_baseline_message_24)
+                        .setContentTitle("My Notification")
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
