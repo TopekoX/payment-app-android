640d845f447c2d99406819cf5c45adcbbdfcc855
Ucup TopekoX <ucup.topekox@gmail.com>
2022-06-14 00:54:47 +0800

    update RegistrasiFCMService

--- app/src/main/java/com/topekox/aplikasipembayaran/service/RegistrasiFCMService.java
+++ app/src/main/java/com/topekox/aplikasipembayaran/service/RegistrasiFCMService.java
@@ -15,27 +15,28 @@ public class RegistrasiFCMService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        Log.w(TAG, "Registrasi FCM Service Is Running...");
-                        String email = intent.getStringExtra("EMAIL");
-                        String token = intent.getStringExtra("TOKEN");
-                        Log.w(TAG, "email: " + email +", token: " + token);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    Log.w(TAG, "Registrasi FCM Service Is Running...");
+                    String email = intent.getStringExtra("EMAIL");
+                    String token = intent.getStringExtra("TOKEN");
+                    Log.w(TAG, "email: " + email + ", token: " + token);
 
-                        try {
-                            if (email != null && token != null) {
-                                PembayaranClient pembayaranClient = new PembayaranClient();
-                                pembayaranClient.registrasiToken(email, token);
-                                Log.w(TAG, "Mengirim Data Token...");
-                            }
-                            Thread.sleep(2000);
-                        } catch (Exception e) {
-                            Log.w(TAG, e.getMessage());
+                    try {
+                        if (email != null && token != null) {
+                            PembayaranClient pembayaranClient = new PembayaranClient();
+                            pembayaranClient.registrasiToken(email, token);
+                            Log.w(TAG, "Mengirim Data Token...");
                         }
+                        Thread.sleep(2000);
+                    } catch (Exception e) {
+                        Log.w(TAG, e.getMessage());
                     }
                 }
+            }
+        }
         ).start();
         return super.onStartCommand(intent, flags, startId);
     }
